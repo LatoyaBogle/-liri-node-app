776ae962a769543541dd64a33ffce4d08157f78b
LatoyaBogle <concordiae20@gmail.com>
2018-10-11 16:36:46 -0400

    upload of files for activity

---
+++ keys.js
@@ -0,0 +1,25 @@
+console.log('this is loaded');
+
+/*exports.twitter = {
+  consumer_key: process.env.mqEa5dCd7HrUhwqst32fKngEa,
+  consumer_secret: process.env.pRY7wdIwo68PkT41MsWQx3OeCKgT6PXoVE3VAWBTLVDa8U9OxS,
+  access_token_key: process.env.FlqgEjFLFze72tnmaUw4G92TTnp0j32ewBtddUq,
+  access_token_secret: process.env.B34pTnShVtSyLP7SP5gD5988d4z6EdKuJteIm0JCsBO8L 
+};
+
+/*exports.spotify = {
+  id: process.env.c646b6ec52ef4b22a018e905e5115fb5,
+  secret: process.env.d5033dd494c8c06746644806a11
+*/
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
